c7fc13096178d44c156fdd2b32535993f6e888e7
EC2 Default User <ec2-user@ip-172-31-11-218.ec2.internal>
2022-01-24 01:00:45 +0000

    my lab2.py file.

---
+++ lab2.py
@@ -0,0 +1,30 @@
+my_name = "Tom"
+print(my_name)
+
+my_id = 123
+print(my_id)
+#123 = "my_id"
+#assign variable to right side
+
+my_id = your_id = 123
+print(my_id)
+print(your_id)
+
+my_id_str = '123'
+print(my_id_str)
+
+#print(my_name + my_id)
+
+print(my_name + my_id_str)
+
+print(my_name * 3)
+
+print("hello, world. This is my first python string.".split("."))
+
+message = "Tom id is 123"
+print(message)
+
+end = "end"
+print(end)
+
+
