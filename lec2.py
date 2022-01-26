d8d09230971f99cea2415151a0c35f91a83c23e3
EC2 Default User <ec2-user@ip-172-31-11-218.ec2.internal>
2022-01-26 21:38:01 +0000

    lecture 2

---
+++ lec2.py
@@ -0,0 +1,26 @@
+""""
+
+this is a regional comment
+
+
+"""""
+
+#print("hello world") this is a comment
+print(type(123))
+print(type(123.))
+print(type("123"))
+print("hello world")
+print("toms cat")
+float = 123.
+int = 123
+print(float + int)
+print("hello world".upper())
+print("hello world".lower())
+print("    hello world    ")
+print("    hello world".strip())
+print("hello world".split())
+print("hello, world".split(","))
+print("hello world".split("o"))
+print(3/3)
+print(3.0+9)
+print(2**3)
