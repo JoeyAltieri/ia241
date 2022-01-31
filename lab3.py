a065a76bc574bf07ecbd863bdcbef57a1d04a80d
EC2 Default User <ec2-user@ip-172-31-11-218.ec2.internal>
2022-01-31 21:14:28 +0000

    lab3.py file

---
+++ lab3.py
@@ -0,0 +1,49 @@
+"""""
+
+lab 3
+
+"""""
+
+str_list = ['a','d','e','b','c',]
+
+print(str_list)
+
+str_list.sort()
+print(str_list)
+
+str_list.append('f')
+print(str_list)
+
+str_list.remove('d')
+print(str_list)
+
+print(str_list[2])
+
+my_list = ['a','123',123,'b','B','False',False,123,None,'None']
+print(len(set(my_list)))
+print(len(my_list))
+
+print(len("This is my third python lab"))
+
+num_list = [12,32,43,35]
+num_list.sort()
+print(num_list[0])
+print(num_list[-1])
+
+game_board = [  [0,0,0],
+                [0,0,0],
+                [0,0,0]]
+
+print(game_board)
+
+game_board[1][1]=1
+print(game_board)
+
+print("end")
+
+
+
+
+
+
+
