858a17e3e5d66f01be66e0f8546a9a18ef0ce3ec
EC2 Default User <ec2-user@ip-172-31-11-218.ec2.internal>
2022-02-24 00:30:06 +0000

    lab 5

---
+++ lab5.py
@@ -0,0 +1,55 @@
+''''
+lab 5
+'''
+
+#3.1
+
+alien_color = 'green'
+
+if alien_color == 'green':
+    print('the player just earned 5 points')
+    
+#3.2
+
+alien_color = 'yellow'
+
+if alien_color == 'green':
+    print('the player just earned 5 points')
+    
+else:
+    print('the player just earned 10 points')
+
+#3.3
+
+favorite_fruits = ['orange', 'apple', 'mango']
+
+if 'apple' in favorite_fruits:
+    print('Apple lover!')
+    
+if 'mango' in favorite_fruits:
+    print('Mango lover!')
+    
+if 'orange' in favorite_fruits:
+    print('Orange lover!')
+    
+#3.4
+
+age = 19
+
+if age < 10: 
+    print('kid')
+elif age < 20:
+    print('teenager')
+else:
+    print("You are an adult")
+    if age > 65:
+        print('you are elder')
+
+
+    
+    
+        
+
+
+
+
